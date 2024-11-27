2da20f55eef36da27b543cbf85dd033ffe98da62
Kelvin Muchiri <muchirikelv71@gmail.com>
2024-11-27 13:11:38 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Morgan Muchiri';
+const welcomeMessage = 'Welcome to Flatbook, Morgan Muchiri!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, MORGAN MUCHIRI!';
+const shortGreeting = 'Welcome, M!';
